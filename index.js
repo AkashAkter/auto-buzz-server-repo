179b368c85d736f71eb9b5867ec596bd1bd1e72a
Akash Akter <AkashAkter>
2022-11-24 18:13:54 +0600

    server side work starts now

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
+
+const port = process.env.PORT || 5000;
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.a23vi3u.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+
+
+app.get('/', async (req, res) => {
+    res.send('Server is Running')
+});
+
+app.listen(port, () => {
+    console.log(`Server running on ${port}`);
+})

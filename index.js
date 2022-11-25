215be0238a31480c8ad5011135d63715ba9560ad
Akash Akter <AkashAkter>
2022-11-26 01:34:51 +0600

    jwt work done

--- index.js
+++ index.js
@@ -8,12 +8,34 @@ const port = process.env.PORT || 5000;
 
 const app = express();
 
+// middleware
 app.use(cors());
 app.use(express.json());
+
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.a23vi3u.mongodb.net/?retryWrites=true&w=majority`;
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+function verifyJWT(req, res, next) {
+
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+        return res.status(401).send('unauthorized access');
+    }
+
+    const token = authHeader.split(' ')[1];
+
+    jwt.verify(token, process.env.ACCESS_TOKEN, function (err, decoded) {
+        if (err) {
+            return res.status(403).send({ message: 'forbidden access' })
+        }
+        req.decoded = decoded;
+        next();
+    })
+
+}
+
 
 async function run() {
     try {
@@ -81,11 +103,28 @@ async function run() {
             res.send(result);
         });
 
-        app.get('/bookings', async (req, res) => {
+        app.get('/bookings', verifyJWT, async (req, res) => {
             const email = req.query.email;
+            const decodedEmail = req.decoded.email;
+
+            if (email !== decodedEmail) {
+                return res.status(403).send({ message: 'forbidden access' });
+            }
+
             const query = { email: email };
             const result = await bookingsCollection.find(query).toArray();
             res.send(result);
+        });
+
+        app.get('/jwt', async (req, res) => {
+            const email = req.query.email;
+            const query = { email: email };
+            const user = await usersCollection.findOne(query);
+            if (user) {
+                const token = jwt.sign({ email }, process.env.ACCESS_TOKEN, { expiresIn: '1h' })
+                return res.send({ accessToken: token });
+            }
+            res.status(403).send({ accessToken: '' })
         });
 
         app.post('/users', async (req, res) => {
@@ -97,6 +136,12 @@ async function run() {
         app.get('/users', async (req, res) => {
             const email = req.query.email;
             const query = { email: email };
+            const result = await usersCollection.find(query).toArray();
+            res.send(result);
+        });
+
+        app.get('/allusers', async (req, res) => {
+            const query = {};
             const result = await usersCollection.find(query).toArray();
             res.send(result);
         });
